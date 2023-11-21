054815d98e5165aab0d78de6666d314a76b37fc1
Erick Schiller <erickschiller@hotmail.com>
2023-11-21 14:54:49 -0600

    Final

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # CifradoImagenes
- 
+
+## Instrucciones
+
+### Requisitos
+
+- Instalación de Python.
+
+### Comandos
+
+Instalar las librerías necesarias.
+
+```bash
+pip install cryptography
+```
+
+Correr el archivo.
+
+```python
+python image_encryption.py
+```
+
+### Pruebas
+
+Utilizar cualquiera de los siguientes links para probar las rutas:
--- image_encryption.py
+++ image_encryption.py
@@ -3,50 +3,70 @@ import codecs
 import binascii
 from cryptography.fernet import Fernet
 
-filename = 'perro.bin'
+# Github para la conversión de la imagen a binario
+    # https://github.com/henriland/image-to-binary/tree/master
 
-def binary_to_image():
+key = Fernet.generate_key()
+    
+# Instance the Fernet class with the key 
+fernet = Fernet(key)
 
+
+# Binario a imagen
+def binary_to_image(filename):
+
+    # Lee el archivo de la imagen cidrada
     binFile = open(filename,'rb')
     binaryData = binFile.read()
-    hexData = '%0*X' % ((len(binaryData) + 3) // 4, int(binaryData, 2))
 
+    # Desencripta la imagen encriptda
+    binaryData = fernet.decrypt(binaryData).decode()
+
+    # Traduce el bianrio a imagen
+    hexData = '%0*X' % ((len(binaryData) + 3) // 4, int(binaryData, 2))
     decode_hex = codecs.getdecoder("hex_codec")
     hexData = decode_hex(hexData)[0]
-
     parsed_filename = os.path.splitext(os.path.basename(filename))[0]
     png_filename = parsed_filename + '_from_binary.png'
+
+    # Crea el archivo de la imagen
     with open(png_filename, 'wb') as file:
         file.write(hexData)
 
 
+# Imagen a binario
+def image_to_binary(filename):
 
-def image_to_binary():
+    # Lee el archivo de la imagen en formato de binario
     with open(filename, 'rb') as file:
         image_data = file.read()
 
+    # Traduce la imagen a binario
     data = binascii.hexlify(image_data)
-
     binary = bin(int(data, 16))
     binary = binary[2:].zfill(32)
 
-    key = Fernet.generate_key()
-    
-    # Instance the Fernet class with the key
-    
-    fernet = Fernet(key)
-    
-    # then use the Fernet class instance 
-    # to encrypt the string string must
-    # be encoded to byte string before encryption
+    # Encripta el código binario de la imagen
     encMessage = fernet.encrypt(binary.encode())
-
     binary = encMessage
-    
 
-
+    # Guarda la imagen encriptada en el archivo nombreImagen.bin
     parsed_filename = os.path.splitext(os.path.basename(filename))[0]
     binary_filename = parsed_filename + '.bin'
     with open(binary_filename, 'wb') as file:
-        # file.write(binary.encode())
         file.write(binary)
+
+
+# filename_cifrado = 'perro.bin'
+# filename_imagen = 'perro.jpeg'
+
+filename_imagen = input("Ingresa el nombre de la imagen (Debe de estar en el mismo directorio): ")
+
+# La imagen cifrada se encuentra en perro.bin
+image_to_binary(filename_imagen)
+
+filename_cifrado = filename_imagen.split(".")[0] + ".bin"
+
+# La imagen descifrada se encuentra en perro.jpeg
+binary_to_image(filename_cifrado)
+
